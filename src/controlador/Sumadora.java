4467ce24447ce6393c901723b629e05c0ffb1fcb
J0rg3-Luis <72526825+J0rg3-Luis@users.noreply.github.com>
2022-03-22 22:49:05 -0600

    Creación de practica

---
+++ src/controlador/Sumadora.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlador;
+
+/**
+ *clase que suma numeros
+ * @author alumno
+ */
+public class Sumadora {
+    /**
+     * constructor vacio
+     */
+
+    public Sumadora(){
+        
+    }
+    /**
+     * Metodo que realiza la suma de dos nuemros
+     * @param a El primer numero a sumar
+     * @param b El segundo numero a sumar 
+     * @return La suma de dos valores double
+     */
+    public double sumar(double a, double b){
+        double suma = a+b;
+        return suma;
+        
+    }
+
+
+}
+
